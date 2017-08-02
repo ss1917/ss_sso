9cc98987b453813ae7592552bed1e5ccd4ec64bc
ss1917 <191715030@qq.com>
2017-08-02 17:12:59 +0800

    跨域

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-go get github.com/astaxie/beego
-go get github.com/beego/bee
-go get github.com/go-sql-driver/mysql
-go get github.com/astaxie/beego/session/redis
-go get github.com/astaxie/beego/httplib
-go get github.com/garyburd/redigo/redis
+### 安装依赖 
+- go get github.com/astaxie/beego 
+- go get github.com/beego/bee 
+- go get github.com/go-sql-driver/mysql 
+- go get github.com/astaxie/beego/session/redis 
+- go get github.com/astaxie/beego/httplib 
+- go get github.com/garyburd/redigo/redis 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/astaxie/beego/session/redis"
 	_ "github.com/ss1917/ss_sso/routers"
 )
@@ -11,5 +12,14 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
+
 	beego.Run()
 }
